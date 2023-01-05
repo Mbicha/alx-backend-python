36423d036cd40f27a621d10568b147af5cc76429
Mbicha <mbithicharlse@gmail.com>
2023-01-05 19:58:07 +0300

    Concatenatetwo strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Concatenate/join two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Args:
+        str1 (str): First String
+        str2 (str): Second String
+    Return:
+        str: Concatenated strings
+    """
+    return (str1 + str2)
