a54680c510049ab92ccbc3b44d7c43905d85b224
Mbicha <mbithicharlse@gmail.com>
2023-01-05 20:21:44 +0300

    String representation of float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Floor of passed float"""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        n (float): Number Float
+    Return:
+        str : String representstion of passed float
+    """
+    return str(n)
