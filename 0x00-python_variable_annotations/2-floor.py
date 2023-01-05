0dbc02a5e12a903931640019e670a081f5e050d2
Mbicha <mbithicharlse@gmail.com>
2023-01-05 20:11:26 +0300

    floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Floor of passed float"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Args:
+        n (float): a number float
+    Return:
+        floor (float): Return floor of a passed number
+    """
+    return math.floor(n)
